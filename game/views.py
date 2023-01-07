4d91621ad41b4796d2b7a2c9222e737a64942246
helilokan <1071154067@qq.com>
2023-01-07 14:21:51 +0800

    create 1st web

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">我的第一个网页</h1>'
+    line2 = '<img src="https://img0.baidu.com/it/u=2109596793,204974589&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"width = 1200>'
+    return HttpResponse(line1+line2)
